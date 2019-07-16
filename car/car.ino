0c9b793485fdfc5aa25ee3e112bffec53dfeaf1c
rtk0c <41214072+rtk0c@users.noreply.github.com>
2019-07-16 16:06:32 -0700

    Fading number class

--- car/car.ino
+++ car/car.ino
@@ -1,5 +1,45 @@
 #include <Servo.h>
 
+class FadingInt {
+  public:
+    FadingInt(int start, int change, int llim, int ulim) {
+      this->num = start;
+      this->llimit = llim;
+      this->ulimit = ulim;
+      this->change = change;
+    }
+    void next() {
+      bool state = this->state;
+      if (state) {
+        // Increasing
+        this->num += this->change;
+      } else {
+        // Decreasing
+        this->num -= this->change;
+      }
+
+      int num = this->num;
+      int llim = this->llimit;
+      int ulim = this->ulimit;
+      if(num <= llim) {
+        this->num = llim;
+        this->state = !state;
+      } else if(num >= ulim) {
+        this->num = ulim;
+        this->state = !state;
+      }
+    }
+    int getNum() {
+      return this->num;
+    }
+  private:
+    int num;
+    int llimit;
+    int ulimit;
+    int change;
+    bool state;
+};
+
 // Pins
 const int RIGHT_ENABLE = 9;
 const int RIGHT_IN1 = 7;
@@ -27,17 +67,14 @@ void setup() {
   sensor.attach(SERVO_SIGNAL);
 }
 
-int pos = 0;
+FadingInt pos(0, 5, 0, 180);
 void loop() {
   triggerSens();
   Serial.print(getDistCM());
   Serial.println(" cm");
 
-  sensor.write(pos);
-  pos += 5;
-  if (pos >= 180) {
-    pos = 0;
-  }
+  sensor.write(pos.getNum());
+  pos.next();
 
   delay(100);
 }
